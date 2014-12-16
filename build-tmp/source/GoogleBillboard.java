ad9d8886fdedb71bc3d819c3f1b750057a6bd574
unknown <vasee@697-334-Bard.sf.k12.internal>
2014-12-16 08:17:55 -0800

    Billboard

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -15,18 +15,38 @@ import java.io.IOException;
 public class GoogleBillboard extends PApplet {
 
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+
 public void setup()  
 {            
-     noLoop();  
+     noLoop();
+
 }  
 public void draw()  
 {   
-	//not needed for this assignment
+	for(int i=2;i<e.length()-10;i++)
+	{
+		String digits = e.substring(i,i+10); 
+        double dNum = Double.parseDouble(digits); 	
+
+        if(isPrime(dNum)==true)
+	{
+		System.out.println(digits);
+		break;
+	}
+	}
+	
 }  
-public boolean isPrime(double dNum)  
+public boolean isPrime(double nNum)  
 {   
-    //to be finished later   
-    return true;  
+  for(int i=2;i<=Math.sqrt(nNum);i++)
+  {
+   if(nNum%i==0)
+  {
+   return false;
+  }
+ }
+ return true;
+
 } 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
